96a36178852050b9de57ba415e963a924b21494b
cdikop <cdikop@163.com>
2021-06-12 10:08:45 +0800

    no message
    Signed-off-by: cdikop <cdikop@163.com>

--- Kaplan-Meier 曲线绘制.R
+++
@@ -1,8 +0,0 @@
-library(survival)    #进行生存分析的包
-library(ggplot2)
-library(survminer)  #生成生存曲线的包
-
-KM <- survfit(Surv(time, status) ~ sex, data = lung, type = 'kaplan-meier', conf.type = 'log')
-# 对数据进行生存分析
-
-ggsurvplot(KM)  #可以绘制出来生存曲线
